49a92ecff350a4e7df5ef7a7c4cce08b50b7062d
Ordboka <bensbuan@gmail.com>
2017-01-20 13:52:05 +0100

    Changed Vehicle, still not done.

--- Ovinger/oving2/Vehicle.java
+++ Ovinger/oving2/Vehicle.java
@@ -7,20 +7,37 @@ public class Vehicle {
 	
 	private boolean legalRegistration(char vehicleType,char fuelType, String registrationNumber){
 		boolean legal = true;
+		if(registrationNumber.substring(0,2).matches("[0-9]+")||registrationNumber.substring(0,2).matches("[a-z]+")){
+			legal = false;
+		}
+		if(Character.isDigit(registrationNumber.charAt(2))==false){
+			legal = false;
+		}
 		if(vehicleType == 'M' &&  registrationNumber.length()!=6){
 			legal = false;
-		}else if(vehicleType == 'C' &&  registrationNumber.length()!=7)
+		}else if(vehicleType == 'C' &&  registrationNumber.length()!=7){
+			legal = false;
+		}
+		if(fuelType == 'E'){
+			if((registrationNumber.startsWith("EL")||registrationNumber.startsWith("EK"))==false){
+				legal = false;
+			}
+		}else if(fuelType == 'H'){
+			if(registrationNumber.startsWith("HY")==false){
+				legal = false;
+			}
+		}
 		return legal;
 	}
 	
 	
 	public Vehicle(char vehicleType, char fuelType, String registrationNumber){
-		if(legalRegistration(fuelType,registrationNumber)==true){
+		if(legalRegistration(vehicleType,fuelType,registrationNumber)==true){
 			this.vehicleType = vehicleType;
 			this.fuelType = fuelType;
 			this.registrationNumber = registrationNumber;
 		}else{
-			throw new IllegalArgumentException("Wrong registrationnumber compared to fueltype");
+			throw new IllegalArgumentException("Wrong registration number compared to fueltype");
 		}
 	}
 	public char getFuelType(){
@@ -30,10 +47,10 @@ public class Vehicle {
 		return registrationNumber;
 	}
 	public void setRegistrationNumber(String s){
-		if(legalRegistration(fuelType,s)==true){
+		if(legalRegistration(vehicleType,fuelType,s)==true){
 			this.registrationNumber = s;
 		}else{
-			throw new IllegalArgumentException("Wrong registrationnumber compared to fueltype");
+			throw new IllegalArgumentException("Wrong registration number compared to fueltype");
 		}
 	}
 	public char getVehicleType(){
